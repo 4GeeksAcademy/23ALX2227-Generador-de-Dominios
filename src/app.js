1b05e84aa8c3c602d0de142ab1362adb559c6ca9
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-09-26 02:46:55 +0000

    Final Dominio

--- src/app.js
+++ src/app.js
@@ -1,45 +1,46 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generador() {
+  const palabras = {
+    pronouns: ["The", "Your", "Our", "Mine", "They"],
+    adjective: [
+      "Funny",
+      "Glamorous",
+      "Ignorant",
+      "Illegal",
+      "Large",
+      "Obese",
+      "Hot"
+    ],
+    nouns: [
+      "Advice",
+      "Website",
+      "Racoon",
+      "Lion",
+      "Band",
+      "Newspapper",
+      "Console",
+      "Call-center",
+      "Assistant",
+      "Bike"
+    ]
+  };
 
-let pronoun = ["yo", "tu", "el", "nosotros", "ellos"];
-let noun = [
-  "mago",
-  "leon",
-  "arbol",
-  "invocador",
-  "geeks",
-  "camper",
-  "gamer",
-  "jugador"
-];
-let adj = [
-  "super",
-  "alto",
-  "fuerte",
-  "incanzable",
-  "bello",
-  "musculoso",
-  "magico",
-  "ancestral"
-];
-let ext = [".es", ".com", ".com.ar", ".com.nz"];
+  let ejemplos = [];
 
-function domainNameGenerator(pronoun, noun, adj, ext) {
-  let numPronoun = Math.floor(Math.random() * pronoun.length);
-  let numNoun = Math.floor(Math.random() * noun.length);
-  let numAdj = Math.floor(Math.random() * adj.length);
-  let numExt = Math.floor(Math.random() * ext.length);
-
-  return pronoun[numPronoun] + noun[numNoun] + adj[numAdj] + ext[numExt];
+  for (i = 0; i < palabras.pronouns.length; i++) {
+    for (j = 0; j < palabras.adjective.length; j++) {
+      for (k = 0; k < palabras.nouns.length; k++) {
+        ejemplos.push(
+          palabras.pronouns[i] +
+            palabras.adjective[j] +
+            palabras.nouns[k] +
+            ".com"
+        );
+      }
+    }
+  }
+  return ejemplos;
 }
 
-function onLoad() {
-  let domainGen = document.getElementById("domainGenerator");
-  domainGen.innerHTML = domainNameGenerator(pronoun, noun, adj, ext);
-}
-
-window.onload = onLoad;
+console.log(generador());
